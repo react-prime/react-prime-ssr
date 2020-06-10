0ec1cbc3081dbd239fe60d1e33aefe8789d4441c
Sander Vispoel <sander.vispoel@labela.nl>
2020-06-10 17:40:58 +0200

    Improve Next page component type to support both standard and Redux component

--- src/types/next.ts
+++ src/types/next.ts
@@ -1,14 +1,20 @@
 import * as i from 'types';
 import { NextPageContext } from 'next';
 import { AppInitialProps } from 'next/app';
+import { O } from 'ts-toolbelt';
 
-export type AugmentedNextPageContext<Q = {}> = NextPageContext & {
+export type AugmentedNextPageContext<Q = {}> = O.Update<NextPageContext, 'query', Q>
+
+export type AugmentedNextPageContextWithRedux<Q = {}> = AugmentedNextPageContext<Q> & {
   store: i.Store;
-  query: Q;
 }
 
-type NextPageComponentReturn = Partial<AppInitialProps>;
+type NextPageComponentReturn<P = {}> = Partial<AppInitialProps> & P;
 
-export type NextPageComponent<P = {}, Q = {}> = React.FC<P> & {
-  getInitialProps?(context: AugmentedNextPageContext<Q>): Promise<NextPageComponentReturn>;
+type NextPageBaseProps<C, P> = React.FC<P> & {
+  getInitialProps?(context: C): Promise<NextPageComponentReturn<P>> | NextPageComponentReturn<P>;
 }
+
+export type NextPageComponent<P = {}, Q = {}> = NextPageBaseProps<AugmentedNextPageContext<Q>, P>;
+
+export type NextPageReduxComponent<P = {}, Q = {}> = NextPageBaseProps<AugmentedNextPageContextWithRedux<Q>, P>;
