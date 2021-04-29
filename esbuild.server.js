69c1fc1d681442757e4dd3d4654c9785e92a08ca
sandervspl <sandervispoel@hotmail.com>
2021-04-29 15:08:08 +0200

    Remove Nodemon, build server with esbuild
    From testing it seems that rebuilding the server is unnecessary in dev, so I removed the Nodemon watcher

---
+++ esbuild.server.js
@@ -0,0 +1,44 @@
+/* eslint-disable @typescript-eslint/no-var-requires, no-console */
+const cp = require('child_process');
+const pkg = require('./package.json');
+
+const fileName = 'server/index';
+const isDev = Boolean(process.env.DEV);
+
+let ls;
+
+function runNextServer() {
+  if (ls) {
+    ls.kill();
+  }
+
+  ls = cp.spawn('node', ['dist/server/index.js'], { stdio: 'inherit' });
+
+  if (ls.stdout) {
+    ls.stdout.on('data', (data) => {
+      process.stdout.write(data);
+    });
+  }
+
+  if (ls.stderr) {
+    ls.stderr.on('data', (data) => {
+      process.stderr.write(data);
+    });
+  }
+}
+
+require('esbuild').build({
+  tsconfig: 'tsconfig.server.json',
+  entryPoints: [`${fileName}.ts`],
+  outfile: `dist/${fileName}.js`,
+  platform: 'node',
+  target: 'node10',
+  bundle: true,
+  external: Object.keys(pkg.dependencies),
+})
+  .catch(() => process.exit(1));
+
+if (isDev) {
+  console.log('⚡️ Server compiled succesfully');
+  runNextServer();
+}
