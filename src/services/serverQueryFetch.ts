cfea8debf5708ca73b9f15d41d936929eb28c54a
sandervspl <sandervispoel@hotmail.com>
2021-12-29 13:22:19 +0100

    Update argument types

--- src/services/serverQueryFetch.ts
+++ src/services/serverQueryFetch.ts
@@ -1,13 +1,11 @@
-import { dehydrate, QueryClient } from 'react-query';
+import * as i from 'types';
+import { dehydrate, QueryClient, QueryKey } from 'react-query';
 
-export const serverQueryFetch = async <Fn extends (...args: unknown[]) => unknown>(
-  key: string | (string | number)[],
-  fetchFunc: Fn,
-) => {
+export const serverQueryFetch = async <Fn extends i.AnyFn>(key: QueryKey, fetchFn: Fn) => {
   // Create a new QueryClient instance for each page request.
   // This ensures that data is not shared between users and requests.
   const queryClient = new QueryClient();
-  await queryClient.prefetchQuery(key, fetchFunc);
+  await queryClient.prefetchQuery(key, fetchFn);
 
   return {
     dehydratedState: dehydrate(queryClient),
